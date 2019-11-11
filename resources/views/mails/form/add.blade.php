1ed2450ab1b69d3e68e48c89f12b97d4ff887bd5
DIPANDA Achile <dipanda27@gmail.com>
2019-11-11 10:48:32 -0800

    Consolidation du formulaire d'ajout de courrier

--- resources/views/mails/form/add.blade.php
+++ resources/views/mails/form/add.blade.php
@@ -24,29 +24,44 @@
     <span class="mb-0 mt-6" id="infoAlert"></span>
     
     <div class="card card-small mb-4">
-      <div class="card-body p-10 pb-3 text-center">
+      <div class="card-body p-10 pb-3">
         <h4 class="text-center" style="padding: 10px;">Enregistrement d'un nouveau courrier</h4> <hr>
 
         <form action="/mails/add" method="POST" class="form" enctype="multipart/form-data">
           {{ csrf_field() }}
           <div class="row">
-            <div class="col col-md-6 col-sm-6 col-xs-12">
-              <h6>Informations du Courrier</h6>
+            <div class="col-5">
+              <h5 class="pb-20">Informations du Courrier</h5>
+                <div class="form-group">
+                  <div class="" style="padding-left: 0px; padding-top: 5px; width: 100%; text-align: left">
+                    <label for="">Doucment joint</label>
+                  </div>
+                  <div class="custom-file">
+                    <input type="file" name="attachment" id="attachment" class="custom-file-input">
+                    <label for="attachment" class="custom-file-label">Choisissez un fichier...</label>
+                  </div>
+                </div>
                 <div class="input-group mb-3">
+                  <div class="" style="padding-left: 0px; padding-top: 5px; width: 100%; text-align: left">
+                    <label for="">Sujet</label>
+                  </div>
                   <div class="input-group-prepend">
                     <span class="input-group-text"><i class="fa fa-heading"></i></span>
                   </div>
-                <input type="text" value="{{ old('subject') }}" class="form-control form-control-lg" placeholder="Objet" name="subject">
+                <input type="text" value="{{ old('subject') }}" class="form-control" placeholder="Objet" name="subject">
                   <div style="width: 100%; text-align: left; color: red">
                     <i>{{ $errors->first('subject') }}</i> 
                   </div>
                 </div>
 
                 <div class="input-group mb-3">
+                  <div class="" style="padding-left: 0px; padding-top: 5px; width: 100%; text-align: left">
+                    <label for="">Nature de la requête</label>
+                  </div>
                   <div class="input-group-prepend">
                     <span class="input-group-text"><i class="fa fa-cog"></i></span>
                   </div>
-                  <select name="type" name="nature" class="form-control form-control-lg" id="nature">
+                  <select name="type" name="nature" class="form-control" id="nature">
                     <option value="" disabled>Choisissez la nature de la requête</option>
                     <option value="request">Requête</option>
                     <option value="demand">Demande</option>
@@ -54,10 +69,13 @@
                   </select>
                 </div>
                 <div class="input-group mb-3">
+                  <div class="" style="padding-left: 0px; padding-top: 5px; width: 100%; text-align: left">
+                    <label for="">Catégorie du courrier</label>
+                  </div>
                   <div class="input-group-prepend">
                     <span class="input-group-text"><i class="fa fa-cog"></i></span>
                   </div>
-                  <select name="type" name="nature" class="form-control form-control-lg" id="nature">
+                  <select name="type" name="nature" class="form-control" id="nature">
                     <option value="" disabled>Choisissez la catégorie</option>
                     <option value="arrived">Courrier arrivé</option>
                     <option value="outgoing">Courrier sortant</option>
@@ -65,10 +83,13 @@
                   </select>
                 </div>
                 <div class="input-group mb-3">
+                  <div class="" style="padding-left: 0px; padding-top: 5px; width: 100%; text-align: left">
+                    <label for="">Priorité</label>
+                  </div>
                   <div class="input-group-prepend">
                     <span class="input-group-text"><i class="fa fa-cog"></i></span>
                   </div>
-                  <select name="type" name="nature" class="form-control form-control-lg" id="nature">
+                  <select name="type" name="nature" class="form-control" id="nature">
                     <option value="" disabled>Priorité du courrier</option>
                     <option value="very_urgent">Très urgent</option>
                     <option value="urgent">Urgent</option>
@@ -105,10 +126,13 @@
                     </div>
                 </div>
                 <div class="input-group mb-3">
+                  <div class="" style="padding-left: 0px; padding-top: 5px; width: 100%; text-align: left">
+                    <label for="">Expéditeur</label>
+                  </div>
                   <div class="input-group-prepend">
                     <span class="input-group-text"><i class="fa fa-user"></i></span>
                   </div>
-                  <select name="type" name="nature" class="form-control form-control-lg" id="nature">
+                  <select name="type" name="expediteur" class="form-control" id="expediteur">
                     <option value="" disabled>Expéditeur</option>
                     <option value="user1">User 1</option>
                     <option value="user2">User 2</option>
@@ -116,58 +140,99 @@
                   </select>
                 </div>
                 <div class="input-group mb-3">
+                  <div class="" style="padding-left: 0px; padding-top: 5px; width: 100%; text-align: left">
+                    <label for="">Destinataire</label>
+                  </div>
+                  <div class="input-group-prepend">
+                    <span class="input-group-text"><i class="fa fa-user"></i></span>
+                  </div>
+                  <select name="destinataire" class="form-control" id="destinataire">
+                    <option value="" disabled>Destinataire</option>
+                    <option value="destinataire1">Destinataire 1</option>
+                    <option value="destinataire2">Destinataire 2</option>
+                    <option value="destinataire3">Destinataire 3</option>
+                  </select>
+                </div>
+                <div class="input-group mb-3">
+                  <div class="" style="padding-left: 0px; padding-top: 5px; width: 100%; text-align: left">
+                    <label for="">Destinataires en copie</label>
+                  </div>
+                  <div class="input-group-prepend">
+                    <span class="input-group-text"><i class="fa fa-user"></i></span>
+                  </div>
+                  <select name="destinataire_en_copie" class="form-control" id="destinataire_en_copie">
+                    <option value="" disabled>Destinataires en copie</option>
+                    <option value="destinataire1">Destinataire 1</option>
+                    <option value="destinataire2">Destinataire 2</option>
+                    <option value="destinataire3">Destinataire 3</option>
+                    <option value="destinataire4">Destinataire 4</option>
+                    <option value="destinataire5">Destinataire 5</option>
+                  </select>
+                </div>
+                <div class="input-group mb-3">
+                  <div class="" style="padding-left: 0px; padding-top: 5px; width: 100%; text-align: left">
+                    <label for="">Service initiateur</label>
+                  </div>
+                  <div class="input-group-prepend">
+                    <span class="input-group-text"><i class="fa fa-user"></i></span>
+                  </div>
+                  <select name="service_initiateur" class="form-control" id="service_initiateur">
+                    <option value="" disabled>Service initiateur</option>
+                    <option value="service1">Service 1</option>
+                    <option value="service2">Service 2</option>
+                    <option value="service3">Service 3</option>
+                    <option value="service4">Service 4</option>
+                    <option value="service5">Service 5</option>
+                  </select>
+                </div>
+                <div class="input-group mb-3">
+                  <div class="" style="padding-left: 0px; padding-top: 5px; width: 100%; text-align: left">
+                    <label for="">Service traitant</label>
+                  </div>
+                  <div class="input-group-prepend">
+                    <span class="input-group-text"><i class="fa fa-user"></i></span>
+                  </div>
+                  <select name="service_traitant" class="form-control" id="service_traitant">
+                    <option value="" disabled>Service traitant</option>
+                    <option value="service1">Service 1</option>
+                    <option value="service2">Service 2</option>
+                    <option value="service3">Service 3</option>
+                    <option value="service4">Service 4</option>
+                    <option value="service5">Service 5</option>
+                  </select>
+                </div>
+                <div class="form-group">
+                  <div class="input-group mb-3" style="text-align: left">
+                      <div class="col" style="padding-left: 0px; padding-top: 5px">
+                        <label for="">Date limite de traitement</label>
+                      </div>
+                      <div class="col" style="padding-right: 5px">
+                          <input class="form-control" data-date-format="dd/mm/yyyy" id="datepicker">
+                      </div>
+                  </div>
+                </div>
+                <div class="input-group mb-3">
+                  <div class="" style="padding-left: 0px; padding-top: 5px; width: 100%; text-align: left">
+                    <label for="">Liste des mots clés</label>
+                  </div>
+                  <div class="input-group-prepend">
+                    <span class="input-group-text"><i class="fa fa-heading"></i></span>
+                  </div>
+                <input type="text" value="{{ old('subject') }}" class="form-control" placeholder="Mot clé" name="subject">
+                  <div style="width: 100%; text-align: left; color: red">
+                    {{-- <i>{{ $errors->first('subject') }}</i>  --}}
+                  </div>
+                </div>
+                <div class="input-group mb-3">
+                  <div class="" style="padding-left: 0px; padding-top: 5px; width: 100%; text-align: left">
+                    <label for="">Détails</label>
+                  </div>
                   <div class="input-group-prepend">
                     <span class="input-group-text"><i class="fa fa-info-circle"></i></span>
                   </div>
-                  <textarea class="form-control form-control-lg" rows="4" name="details">
+                  <textarea class="form-control" rows="4" name="details">
                       {{ old('details') }}
                   </textarea>
-                </div>
-                <div class="custom-file">
-                  <input type="file" name="attachment" id="attachment" class="custom-file-input">
-                  <label for="attachment" class="custom-file-label">Choisissez un fichier...</label>
-                </div>
-              </div>
-
-              
-              <div class="col col-md-6 col-sm-6 col-xs-12">
-                <h6>Informations de l'Expéditeur</h6>
-                <div class="input-group mb-3">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text"> <i class="fa fa-user"></i> </span>
-                  </div>
-                  <input type="text" class="form-control form-control-lg" placeholder="Noms et Prénoms" aria-label="Username" aria-describedby="basic-addon1">
-                </div>
-                <div class="input-group mb-3">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text"><i class="fa fa-envelope"></i></span>
-                  </div>
-                  <input type="text" class="form-control form-control-lg" placeholder="Adresse Email" aria-label="Username" aria-describedby="basic-addon1">
-                </div>
-                <div class="input-group mb-3">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text"><i class="fa fa-map-marker-alt"></i></span>
-                  </div>
-                  <input type="text" class="form-control form-control-lg" placeholder="Adresse" aria-label="Username" aria-describedby="basic-addon1">
-                </div>
-                <div class="input-group mb-3">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text"><i class="fa fa-phone"></i></span>
-                  </div>
-                  <input type="text" class="form-control form-control-lg" placeholder="Téléphone" aria-label="Username" aria-describedby="basic-addon1">
-                </div>
-                <div class="input-group mb-3">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text"><i class="fa fa-mail-bulk"></i></span>
-                  </div>
-                  <input type="text" class="form-control form-control-lg" placeholder="Boite Postale" aria-label="Username" aria-describedby="basic-addon1">
-                </div>
-                <br>
-                <div class="row">
-                  <div class="col"  align="right">
-                    <a href="{{ route('all_mails') }}" class="btn btn-danger">Annuler</a> &nbsp; &nbsp;
-                    <button type="submit" class="btn btn-primary">Enregistrer</button>
-                  </div>
                 </div>
               </div>
             </div>
